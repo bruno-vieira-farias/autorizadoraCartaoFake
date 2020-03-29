5951bebef539d6a346a8c51ffe3b76f75ae040af
Bruno Farias <contato.brunofarias@gmail.com>
2020-03-28 22:03:16 -0300

    Gerando uma massa de 1300 transacoes.

--- src/main/java/br/com/fiap/autorizadoraCartaoFake/autorizadoraCartaoFake/Scheduler/CargaInicialScheduler.java
+++ src/main/java/br/com/fiap/autorizadoraCartaoFake/autorizadoraCartaoFake/Scheduler/CargaInicialScheduler.java
@@ -1,19 +1,19 @@
-//package br.com.fiap.autorizadoraCartaoFake.autorizadoraCartaoFake.Scheduler;
-//
-//import br.com.fiap.autorizadoraCartaoFake.autorizadoraCartaoFake.service.CargaInicialService;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class CargaInicialScheduler {
-//    private final CargaInicialService cargaInicialService;
-//
-//    public CargaInicialScheduler(CargaInicialService cargaInicialService) {
-//        this.cargaInicialService = cargaInicialService;
-//    }
-//
-//    @Scheduled(cron = "*/5 * * * * ?")
-//    public void execute() {
-////        cargaInicialService.enviaCargaInicial();
-//    }
-//}
+package br.com.fiap.autorizadoraCartaoFake.autorizadoraCartaoFake.Scheduler;
+
+import br.com.fiap.autorizadoraCartaoFake.autorizadoraCartaoFake.service.CargaInicialService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CargaInicialScheduler {
+    private final CargaInicialService cargaInicialService;
+
+    public CargaInicialScheduler(CargaInicialService cargaInicialService) {
+        this.cargaInicialService = cargaInicialService;
+    }
+
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void execute() {
+        System.out.println("Rodei");
+    }
+}
--- src/main/java/br/com/fiap/autorizadoraCartaoFake/autorizadoraCartaoFake/domain/TransacaoRepository.java
+++ src/main/java/br/com/fiap/autorizadoraCartaoFake/autorizadoraCartaoFake/domain/TransacaoRepository.java
@@ -1,21 +1,54 @@
 package br.com.fiap.autorizadoraCartaoFake.autorizadoraCartaoFake.domain;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class TransacaoRepository {
 
     public List<Transacao> getCargaInicialTransacoes() {
-        return Arrays.asList(
-                new Transacao(1, LocalDateTime.now(), BigDecimal.valueOf(10.0), StatusTransacao.AUTORIZADA, "", 4000000000000001L),
-                new Transacao(2, LocalDateTime.now(), BigDecimal.valueOf(10.0), StatusTransacao.AUTORIZADA, "", 4000000000000002L),
-                new Transacao(3, LocalDateTime.now(), BigDecimal.valueOf(10.0), StatusTransacao.AUTORIZADA, "", 4000000000000003L),
-                new Transacao(4, LocalDateTime.now(), BigDecimal.valueOf(10.0), StatusTransacao.AUTORIZADA, "", 4000000000000004L)
-        );
+        List<Transacao> transacoes = new ArrayList<>();
+        Random gerador = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            transacoes.add(new Transacao(
+                    i,
+                    LocalDateTime.now(),
+                    BigDecimal.valueOf(gerador.nextDouble() * 1000).setScale(2, BigDecimal.ROUND_HALF_UP),
+                    StatusTransacao.AUTORIZADA,
+                    RandomStringUtils.random(15, true, true),
+                    gerador.nextInt(90) + 10 + 4000000000000000L)
+            );
+        }
+
+        for (int i = 1000; i < 1200; i++) {
+            transacoes.add(new Transacao(
+                    i,
+                    LocalDateTime.now(),
+                    BigDecimal.valueOf(gerador.nextDouble() * 1000).setScale(2, BigDecimal.ROUND_HALF_UP),
+                    StatusTransacao.NAO_AUTORIZADA,
+                    RandomStringUtils.random(15, true, true),
+                    gerador.nextInt(90) + 10 + 4000000000000000L)
+            );
+        }
+
+        for (int i = 1200; i < 1300; i++) {
+            transacoes.add(new Transacao(
+                    i,
+                    LocalDateTime.now(),
+                    BigDecimal.valueOf(gerador.nextDouble() * 1000).setScale(2, BigDecimal.ROUND_HALF_UP),
+                    StatusTransacao.CANCELADA,
+                    RandomStringUtils.random(15, true, true),
+                    gerador.nextInt(90) + 10 + 4000000000000000L)
+            );
+        }
+
+        return transacoes;
     }
 }
